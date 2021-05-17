4987bf8df0afd4c73cb488e3eac16df84f2aaf9d
SimonSkade <simon.skade@gmx.net>
2021-05-17 21:10:28 +0200

    some more simple setup stuff.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import routes from "./api/routes.js"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 
 dotenv.config();
 const port = process.env.PORT || 8000;
@@ -14,7 +15,13 @@ app.use(express.json());
 app.use("/api", routes)
 app.use("*", (req, res) => res.status(404).json({error: "Not fount"}));
 
+app.get("/", (req,res) => {
+    res.send("Yes it works. Access the api with specific calls to /api .");
+});
+
+mongoose.connect(process.env.DATABASE_URI, {useNewUrlParser: true}, () => console.log("connected to DB!"));
+
 app.listen(port, () => {
     console.log("listening on port ", port);
-})
+});
 
