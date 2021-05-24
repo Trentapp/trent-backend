8c903b0f6a8b93178742f512a42209bebd263b74
SimonSkade <simon.skade@gmx.net>
2021-05-24 14:28:27 +0200

    changed patch to put

--- api/routes.js
+++ api/routes.js
@@ -78,15 +78,9 @@ router.delete("/products/product/delete/:productId", async (req,res) => {
 });
 
 //Update a specific product
-router.patch("/products/product/update/:productId", async (req,res) => { //I should change that one to a put method, as the entire product is updated
+router.put("/products/product/update/:productId", async (req,res) => {
     try {
-        const updatedProduct = await Product.updateOne({_id: req.params.productId}, 
-            {$set: {
-                name: req.body.name,
-                desc: req.body.desc,
-                pricePerHour: req.body.pricePerHour,
-                pricePerDay: req.body.pricePerDay,
-            }});
+        const updatedProduct = await Product.replaceOne({_id: req.params.productId}, req.body);
         res.status(200).json(updatedProduct);
     } catch(e) {
         res.status(500).json({message: e});
