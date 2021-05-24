4613e501373f99fb7a08fc4c56af8f2d5aea61c4
SimonSkade <simon.skade@gmx.net>
2021-05-24 15:27:01 +0200

    minor changes

--- api/routes.js
+++ api/routes.js
@@ -80,8 +80,8 @@ router.delete("/products/product/delete/:productId", async (req,res) => {
 //Update a specific product
 router.put("/products/product/update/:productId", async (req,res) => {
     try {
-        const updatedProduct = await Product.replaceOne({_id: req.params.productId}, req.body);
-        res.status(200).json(updatedProduct);
+        await Product.replaceOne({_id: req.params.productId}, req.body);
+        res.status(200).json({status: "success"}); // maybe rather return the edited product, but Product.replaceOne does not return that.
     } catch(e) {
         res.status(500).json({message: e});
     }
