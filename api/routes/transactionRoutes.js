a1e515d5a0cd7162c6ca8f803a23c296f4e980d8
SimonSkade <simon.skade@gmx.net>
2021-06-25 14:36:14 +0200

    made changes so it works now

--- api/routes/transactionRoutes.js
+++ api/routes/transactionRoutes.js
@@ -71,23 +71,19 @@ transactionRouter.post("/sendRequest", async (req, res) => {//I would change the
 	try {
 		if (!req.body.user_uid || !req.body.product_id || !req.body.start_date || !req.body.end_date) { throw "Missing parameters"; }
 
-		const user_result = await User.find({ uid: req.body.user_uid });
-		const user = user_result[0];
+		const user = await User.findOne({ uid: req.body.user_uid });
 		const user_id = user._id;
 		if (!user_id) { throw "User uid not found" }
-
 		const product = await Product.findById(req.body.product_id);
 		const lender_id = product.user_id;
 		if (!lender_id) { console.log("Lender id not found"); throw "Lender id not found"; }
-
 		if (lender_id == user_id) { console.log("Invalid operation: Lender can not be the same user as borrower"); throw "Invalid operation: Lender can not be the same user as borrower" }
-
-		const item = await Product.findById(transaction.item);
-		const diffMilliSeconds = req.body.end_date - req.body.start_date;
+		const [d_end, d_start] = [new Date(req.body.end_date), new Date(req.body.start_date)];
+		const diffMilliSeconds = d_end - d_start;
 		if (diffMilliSeconds < 0){
 			throw "Start Date must be before End Date";
 		}
-		const total_price = Math.ceil(diffMilliSeconds/(1000*60*60*24)) * item.prices.perDay;
+		const total_price = Math.ceil(diffMilliSeconds/(1000*60*60*24)) * product.prices.perDay;
 		//const total_price_per_Hour = (diffMilliseconds/(1000*60*60)) * item.prices.perHour;
 
 		const transaction = {
@@ -102,8 +98,8 @@ transactionRouter.post("/sendRequest", async (req, res) => {//I would change the
 
 		const newTransaction = await Transaction.create(transaction);
 
-		await User.findByIdAndUpdate(user_id, { $push: { transactions_borrower: newTransaction._id } })
-		await User.findByIdAndUpdate(lender_id, { $push: { transactions_lender: newTransaction._id } })
+		await User.updateOne({_id: user_id}, { $push: { transactions_borrower: newTransaction._id } });
+		await User.updateOne({_id: lender_id}, { $push: { transactions_lender: newTransaction._id } });
 
 		res.status(200).json({ status: "success" });
 	} catch (e) {
