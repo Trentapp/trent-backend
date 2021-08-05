6e43a810f1f94578e214328c78c58c4de8aad4e8
Fynn <fynnkiwitt@yahoo.de>
2021-08-06 00:15:50 +0200

    sending _id of user as object for inventory

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -31,11 +31,11 @@ userRouter.post("/create", async (req, res) => {
 userRouter.post("/user", async (req, res) => {
   Logger.shared.log(`Getting private user profile`);
     try {
-        const user = await User.findOne({ uid: req.body.uid }).populate([{path:'inventory', model:'Product', select:['name', 'prices', 'thumbnail']}, {path:'user', model:'User', select:['name', '_id']}]).orFail();
+        const user = await User.findOne({ uid: req.body.uid }).populate([{path:'inventory', model:'Product', select:['name', 'prices', 'thumbnail', 'user'], populate: {path: 'user', model: 'User', select:['_id']}}]).orFail();
         Logger.shared.log(`Successfully got private user profile with id ${user._id}`);
         res.status(200).json(user);
     } catch (e) {
-        Logger.shared.log(`Failed getting private user profile`, 1);
+        Logger.shared.log(`Failed getting private user profile ${e}`, 1);
         res.status(500).json({ message: e });
     }
 });
