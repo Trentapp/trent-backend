57c69a1e7c3aa47b2233e3c8980c2659e26ca59d
Fynn <fynnkiwitt@yahoo.de>
2021-08-09 17:59:10 +0200

    MangoPayClient addUser

--- MangoPayClient.js
+++ MangoPayClient.js
@@ -1,13 +1,37 @@
-import api from "mangopay2-nodejs-sdk"
+import mangopay from "mangopay2-nodejs-sdk"
+import dotenv from "dotenv"
+
+import User from "./api/models/User.js"
 
 class MangoPayClient {
 
 	static shared = new MangoPayClient();
 
 	constructor() {
+		dotenv.config();
+
+		this.api = new mangopay({
+    	clientId: process.env.MANGOPAY_CLIENT_ID,
+    	clientApiKey: process.env.MANGOPAY_API_KEY
+		});
 	}
 
-	sendPushNotification(title, message, tokens){
+	createNewUser (uid, firstName, lastName, birthday, nationality, countryOfResidence, mail) {
+		this.api.Users.create({
+	    PersonType: "NATURAL",
+	    FirstName: firstName,
+	    LastName: lastName,
+	    Birthday: birthday,
+	    Nationality: nationality,
+	    CountryOfResidence: "DE",
+	    Email: "max@mustermann.de",
+		}).then(async function (response) {
+			console.log(response.Id);
+    	console.log("Natural user created", response);
+			const user = await User.findOne({ uid: uid });
+			user.mangopayId = response.Id;
+			await User.replaceOne({ uid : uid }, user);
+		});
 	}
 
 }
