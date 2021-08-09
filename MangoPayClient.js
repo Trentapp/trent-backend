f3077086185893b31d1359c5d2914ebcc23fe559
Fynn <fynnkiwitt@yahoo.de>
2021-08-09 12:47:41 +0200

    added MangoPayClient

---
+++ MangoPayClient.js
@@ -0,0 +1,15 @@
+import api from "mangopay2-nodejs-sdk"
+
+class MangoPayClient {
+
+	static shared = new MangoPayClient();
+
+	constructor() {
+	}
+
+	sendPushNotification(title, message, tokens){
+	}
+
+}
+
+export default MangoPayClient
