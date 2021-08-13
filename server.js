5191b99aa5d21ea25a38f9fa44738f558db5697d
SimonSkade <simon.skade@gmx.net>
2021-08-13 20:59:21 +0200

    hotfix try

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE, PUT, PATCH, POST');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if ('OPTIONS' == req.method) {
         res.sendStatus(200);
