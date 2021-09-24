b64a0e427d863ba1365b35bdf65933547bb6aca6
SimonSkade <simon.skade@gmx.net>
2021-09-24 09:19:02 +0200

    try to fix CORS error

--- server.js
+++ server.js
@@ -12,17 +12,17 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(cors());
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'GET, DELETE, PUT, PATCH, POST');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     if ('OPTIONS' == req.method) {
-//         res.sendStatus(200);
-//     }
-//     else {
-//         next();
-//     }
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, DELETE, PUT, PATCH, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
+});
 app.use(express.json());
 
 app.get("/.well-known/apple-app-site-association", (req, res) => {
